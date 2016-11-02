f8970fe02e010815216640e93d005e9d42adf2dd
Matt Sharp <matthewsharp123@gmail.com>
2016-11-02 11:04:20 +0000

    add recursive call to get bad descriptions

--- src/index.js
+++ src/index.js
@@ -44,32 +44,66 @@ function queryEbay() {
   url += "&itemFilter(0).name=MaxPrice";
   url += "&itemFilter(0).value=1";
   url += "&itemFilter(1).name=EndTimeFrom";
-  url += "&itemFilter(1).value=2016-11-04T16:00:00.768Z";
+  url += "&itemFilter(1).value=2016-11-02T16:00:00.768Z";
   url += "&itemFilter(2).name=EndTimeTo";
-  url += "&itemFilter(2).value=2016-11-04T20:00:00.768Z";
+  url += "&itemFilter(2).value=2016-11-02T20:00:00.768Z";
   url += "&itemFilter(3).name=LocatedIn";
   url += "&itemFilter(3).value=GB";
 
   request
     .get(url)
     .end((err, res) => {
-      //console.log(Object.keys(res))
-      //console.log(JSON.parse(res.text).findItemsAdvancedResponse[0].searchResult[0].item[0].sellingStatus[0].currentPrice)
-      //console.log(JSON.parse(res.text).findItemsAdvancedResponse[0].searchResult[0].item[0])
-      const itemId = JSON.parse(res.text).findItemsAdvancedResponse[0].searchResult[0].item[10].itemId[0];
-      getItem(appId, 'Description,ItemSpecifics', itemId)
+
+      const items = JSON.parse(res.text).findItemsAdvancedResponse[0].searchResult[0].item
+      getBadDescriptions([], items);
     });
 };
 
-function getItem(appId, selectors, itemId, callback) {
-  let url = "http://open.api.ebay.com/shopping";
-  url += "?callname=GetSingleItem";
-  url += "&responseencoding=JSON";
-  url += "&appid=" + appId;
-  url += "&siteid=0";
-  url += "&version=967";
-  url += "&ItemID=" + itemId;
-  url += "&IncludeSelector=" + selectors;
+function getBadDescriptions(currentBadDescriptions, possibleItems) {
+  if (possibleItems.length > 4) {
+    const currentCall = possibleItems.slice(0, 5);
+    let promises = currentCall.map((el) => {
+      return new Promise((resolve, reject) => {
+        const itemId = el.itemId[0];
+
+        const singleItemRoot = 'http://open.api.ebay.com/shopping';
+        const singleItemParams = {
+          'callname': 'GetSingleItem',
+          'responseencoding': 'JSON',
+          'appid': appId,
+          'siteid': '0',
+          'version': '967',
+          'ItemID': itemId,
+          'IncludeSelector': 'Description,ItemSpecifics'        
+        }
+        const singleItemUrl = constructURL(singleItemRoot, singleItemParams);
+        request
+          .get(singleItemUrl)
+          .end((err, res) => {
+            if(err) reject(err);
+            resolve(JSON.parse(res.text));
+          })
+      })
+    }) 
+    Promise.all(promises).then((res) => {
+      const nextBadDescriptions = res.reduce((acc, el) => {
+        const rawDescription = el.Item.Description;
+        const description = cleanDescription(rawDescription);
+        return description.length > 50 ? acc : acc.concat([description]);
+      }, currentBadDescriptions)
+      if (nextBadDescriptions.length > 4) {
+        console.log(nextBadDescriptions);
+      } else {
+        getBadDescriptions(nextBadDescriptions, possibleItems.slice(5))
+      }
+
+    })
+  } else {
+    console.log(currentBadDescriptions)
+  }
+};
+
+function getSingleItem(url, callback) {
   request
     .get(url)
     .end((err, res) => {
@@ -80,7 +114,11 @@ function getItem(appId, selectors, itemId, callback) {
     })
 }
 
-
+function constructURL(root, params) {
+  return Object.keys(params).reduce((acc, el, index) => {
+    return index === 0 ? `${acc}?${el}=${params[el]}` : `${acc}&${el}=${params[el]}`;
+  }, root)
+}
 
 function cleanDescription(rawDescription) {
   if (rawDescription.indexOf('<style') > -1) {
